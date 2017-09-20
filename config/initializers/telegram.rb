fc982ba7c6da54c3116370dbeccd88bce6901b99
Deuteu <Deuteu@users.noreply.github.com>
2017-09-20 21:49:31 +0200

    Fix initializer Telegram.

--- config/initializers/telegram.rb
+++ config/initializers/telegram.rb
@@ -7,8 +7,8 @@ TELEGRAM_CLIENT = TelegramAPI.new(ENV['TG_API_TOKEN'].to_s)
 # https://devcenter.heroku.com/articles/dyno-metadata
 unless ENV['HEROKU_APP_NAME'].nil?
   wh_base_url   = "https://#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
-  response      = api.setWebhook("#{wh_base_url}/#{ENV['TG_WEBHOOK_TOKEN']}").to_json
+  response      = TELEGRAM_CLIENT.setWebhook("#{wh_base_url}/#{ENV['TG_WEBHOOK_TOKEN']}").to_json
   text_response = "Webhook set on '#{wh_base_url}': #{response}"
-  api.sendMessage(ENV['TG_SUPER_ADMIN_ID'].to_s, text_response) unless ENV['TG_SUPER_ADMIN_ID'].nil?
+  TELEGRAM_CLIENT.sendMessage(ENV['TG_SUPER_ADMIN_ID'].to_s, text_response) unless ENV['TG_SUPER_ADMIN_ID'].nil?
   puts text_response
 end
