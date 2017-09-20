43680217cc0def2c79dabddbd59572d021195c36
Deuteu <Deuteu@users.noreply.github.com>
2017-09-20 22:45:05 +0200

    Fix telegram webhook route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
 
-  get ENV['TG_WEBHOOK_TOKEN'] || 'TG_WEBHOOK_TOKEN', to: 'telegram#webhook'
+  get '/:token', to: 'telegram#webhook'
 end
