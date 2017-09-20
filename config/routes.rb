491bb6edd47da904efeab93a7af379df2f24af7f
Deuteu <Deuteu@users.noreply.github.com>
2017-09-20 22:20:22 +0200

    Add telegram web token route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
+
+  get ENV['TG_WEBHOOK_TOKEN'] || 'TG_WEBHOOK_TOKEN', to: 'telegram#webhook'
 end
