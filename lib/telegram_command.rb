d67fe61bfa5eee79172b8b2c2b5fdf4d5c19c6cd
Deuteu <Deuteu@users.noreply.github.com>
2017-10-09 00:06:47 +0200

    Add TelegramCommandStub.

--- lib/telegram_command.rb
+++ lib/telegram_command.rb
@@ -1,4 +1,10 @@
 module TelegramCommand
 end
 
+class TelegramCommandStub
+  def self.command
+    (self.name.split('::').last || self.name).underscore
+  end
+end
+
 Dir[Rails.root.join('lib', 'telegram_command', '*')].each {|file| require file }
