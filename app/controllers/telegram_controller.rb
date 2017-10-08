f1102888c70893bd49288b18e4d097423a52bb2b
Deuteu <Deuteu@users.noreply.github.com>
2017-10-08 16:34:09 +0200

    Improve syntax in TelegramController.

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -10,24 +10,24 @@ class TelegramController < ApplicationController
       render status: :ok, json: {}
       return
     end
-
     message = params[:message]
-    unless message["chat"] && message["chat"]["id"]
+
+    unless message['chat'] && message['chat']['id']
       puts "MissingArgs - No chat id for response: #{params}"
       render status: :ok, json: {}
       return
     end
-    chat_id = message["chat"]["id"]
+    chat_id = message['chat']['id']
 
-    from = message["from"]
-    unless is_admin?(from["id"])
+    from = message['from']
+    unless is_admin?(from['id'])
       puts "NotAdmin - Message by not admin user: #{from}"
-      api.sendMessage(message["chat"]["id"], "My mum told me not to talk to stranger.")
+      TELEGRAM_CLIENT.sendMessage(chat_id, 'My mum told me not to talk to stranger.')
       render status: :ok, json: {}
       return
     end
 
-    unless message["text"]
+    unless message['text']
       puts "EmptyMessage - Message with no text: #{message}"
       render status: :ok, json: {}
       return
@@ -43,6 +43,6 @@ class TelegramController < ApplicationController
   def is_admin?(user_id)
     admin_ids = ENV['TG_ADMIN_IDS'].to_s.split(';')
     admin_ids << ENV['TG_SUPER_ADMIN_ID'].to_s
-    return admin_ids.include?(user_id.to_s)
+    admin_ids.include?(user_id.to_s)
   end
 end
