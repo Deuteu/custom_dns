7b7470ea392fd3b495c72b1c996a32aa42925073
Deuteu <Deuteu@users.noreply.github.com>
2017-10-10 01:30:27 +0200

    Init TelegramCommand:TelegramUser:SuperAdmin.

--- lib/telegram_command/telegram_user.rb
+++ lib/telegram_command/telegram_user.rb
@@ -1,4 +1,17 @@
 class TelegramCommand
   class TelegramUser < AbstractTelegramCommand
+    def self.command
+      'user'
+    end
+
+    class SuperAdmin < AbstractTelegramCommand
+      def self.command
+        'super'
+      end
+
+      def self.exec
+        'Dt'
+      end
+    end
   end
 end
