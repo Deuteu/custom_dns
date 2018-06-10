0b9315d918917a0a5804c0d8b3be0a3c06f0ce5f
Deuteu <Deuteu@users.noreply.github.com>
2018-06-10 21:35:46 +0200

    Edit syntax in TelegramCommand.

--- lib/telegram_command.rb
+++ lib/telegram_command.rb
@@ -1,54 +1,59 @@
 class AbstractTelegramCommand
-  def self.command
-    (self.name.split('::').last || self.name).underscore
-  end
+  class << self
+    attr_accessor :sub_commands
 
-  class << self; attr_accessor :sub_commands end
-  def self.sub_commands
-    @sub_commands ||= self.constants.inject({}) do |hash, c|
-      const = self.const_get(c)
-      if const.is_a?(Class) && const < AbstractTelegramCommand
-        command              = const.command
-        hash[command.to_sym] = const
-      end
-      hash
+    def command
+      (self.name.split('::').last || self.name).underscore
     end
-  end
 
-  def self.exec(args = [], options = {})
-    if self.sub_commands.blank?
-      'Unimplemented command'
-    else
-      sub = args.shift
-      if sub
-        sub.downcase!
-        cmd = @sub_commands[sub.to_sym]
-        if cmd
-          cmd.exec(args, options)
-        else
-          "Unknown subcommand: #{sub}"
+    def sub_commands
+      @sub_commands ||= constants.inject({}) do |hash, c|
+        const = const_get(c)
+        if const.is_a?(Class) && const < AbstractTelegramCommand
+          command              = const.command
+          hash[command.to_sym] = const
         end
+        hash
+      end
+    end
+
+    def exec(args = [], options = {})
+      if sub_commands.blank?
+        'Unimplemented command'
       else
-        "Available subcommands: #{sub_commands.keys.join(',')}"
+        sub = args.shift
+        if sub
+          sub.downcase!
+          cmd = sub_commands[sub.to_sym]
+          if cmd
+            cmd.exec(args, options)
+          else
+            "Unknown subcommand: #{sub}"
+          end
+        else
+          "Available subcommands: #{sub_commands.keys.join(',')}"
+        end
       end
     end
   end
 end
 
 class TelegramCommand < AbstractTelegramCommand
-  def self.exec(args = [], options = {})
-    if self.sub_commands.blank?
-      'Nothing to do here. Sorry.'
-    else
-      sub = args.shift
-      if sub
-        sub.downcase!
-        cmd = @sub_commands[sub.to_sym]
-        if cmd
-          return cmd.exec(args, options)
+  class << self
+    def exec(args = [], options = {})
+      if sub_commands.blank?
+        'Nothing to do here. Sorry.'
+      else
+        sub = args.shift
+        if sub
+          sub.downcase!
+          cmd = sub_commands[sub.to_sym]
+          if cmd
+            return cmd.exec(args, options)
+          end
         end
+        "Available commands: #{sub_commands.keys.join(',')}"
       end
-      "Available commands: #{sub_commands.keys.join(',')}"
     end
   end
 end
--- lib/telegram_command/telegram_user.rb
+++ lib/telegram_command/telegram_user.rb
@@ -1,45 +1,51 @@
 class TelegramCommand
   class TelegramUser < AbstractTelegramCommand
-    def self.command
-      'user'
-    end
-
-    class SuperAdmin < AbstractTelegramCommand
-      def self.command
-        'super'
-      end
-
-      def self.exec(_args = [], _options = {})
-        'Dt'
+    class << self
+      def command
+        'user'
       end
     end
 
     class SuperAdmin < AbstractTelegramCommand
-      def self.command
-        'list'
+      class << self
+        def command
+          'super'
+        end
+
+        def exec(_args = [], _options = {})
+          'Dt'
+        end
       end
+    end
 
-      def self.exec(args = [], _options = {})
-        limit = 5
-        if args.first
-          limit_arg = args.first.to_i
-          limit = limit_arg > 0 && limit_arg < 10 ? limit_arg : limit
+    class List < AbstractTelegramCommand
+      class << self
+        def command
+          'list'
         end
 
-        page_count = TgUser.page(1).per(limit).total_pages
-        page = 1
-        if args.second
-          page_arg = args.second.to_i
-          page = page_arg > 0 && page_arg <= page_count ? page_arg : page
+        def exec(args = [], _options = {})
+          limit = 5
+          if args.first
+            limit_arg = args.first.to_i
+            limit = limit_arg > 0 && limit_arg < 10 ? limit_arg : limit
+          end
+
+          page_count = TgUser.page(1).per(limit).total_pages
+          page = 1
+          if args.second
+            page_arg = args.second.to_i
+            page = page_arg > 0 && page_arg <= page_count ? page_arg : page
+          end
+
+          result = "User list #{page}/#{page_count}"
+
+          TgUser.page(page).per(limit).each do |user|
+            result += "\n- #{user.to_s}"
+          end
+
+          result
         end
-
-        result = "User list #{page}/#{page_count}"
-
-        TgUser.page(page).per(limit).each do |user|
-          result += "\n- #{user.to_s}"
-        end
-
-        result
       end
     end
   end
